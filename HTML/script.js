d06263f2edfc6ddffbdd43a891dd5dc8afd65d65
Incarrnati0n <horvathimi2001@gmail.com>
2024-03-17 16:28:55 +0100

    stuff

--- HTML/script.js
+++ HTML/script.js
@@ -4,7 +4,7 @@ function newPicture() {
   let randomInt = Math.floor(Math.random() * 10) + 1;
   document.getElementById(
     "meme-div"
-  ).innerHTML = `<img src='http://127.0.0.1:5500/FrontEnd/src/meme${randomInt}.png'>`;
+  ).innerHTML = `<img src='http://127.0.0.1:5500/HTML/src/meme${randomInt}.png'>`;
 }
 
 document.getElementById("new-meme").addEventListener("click", newPicture);
