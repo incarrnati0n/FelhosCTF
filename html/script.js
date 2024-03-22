4ca2e4dad373de2bb6ab0cf301d7831a068d722b
Incarrnati0n <horvathimi2001@gmail.com>
2024-03-22 21:16:48 +0100

    h edtherhe

--- html/script.js
+++ html/script.js
@@ -4,7 +4,7 @@ function newPicture() {
   let randomInt = Math.floor(Math.random() * 10) + 1;
   document.getElementById(
     "meme-div"
-  ).innerHTML = `<img src='/images/meme${randomInt}.png'>`;
+  ).innerHTML = `<img src='images/meme${randomInt}.png'>`;
 }
 
 document.getElementById("new-meme").addEventListener("click", newPicture);
